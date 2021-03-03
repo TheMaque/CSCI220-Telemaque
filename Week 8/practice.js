b16da3c0625f57fa76ebaaecf8a847e6ae2c7327
Reginald Telemaque <reggiemaque@gmail.com>
2021-03-03 17:47:25 -0500

    yer

---
+++ Week 8/practice.js
@@ -0,0 +1,21 @@
+class Dog 
+{
+
+    constructor () 
+    {
+        this.breed;
+    }
+    
+    set Description()
+    {
+
+    }
+
+    
+    get Description()
+    {
+        "{name} is a {color} {breed}"
+    }
+
+
+    
