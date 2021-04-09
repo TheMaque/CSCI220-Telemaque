5cc9f616ddda750cc35ae7451bda6effa7a05864
Reginald Telemaque <reggiemaque@gmail.com>
2021-04-09 12:45:14 -0400

    Changes :)

--- Week 12/scriptEmployee Information_Telemaque.js
+++ Week 12/scriptEmployee Information_Telemaque.js
@@ -1,16 +1,12 @@
-
-
-
-  
 $(document).ready(function(){
     $("#submit").click(function()
     {
-      var name = $.trim($("#name").val());
-       var title = $.trim($("#title").val());
+      var name = ($("#name").val());
+       var title = ($("#title").val());
        var hiredate = $("#hiredate").val();
        
 
-       //adds new table row 
+       // Adds a new row of data for the employee.
        if(name ==="" || title ==="" ||hiredate ==="")
        {
            
@@ -73,39 +69,5 @@ $('.sortable').each(function() {
       
 });
 
-$('.sortable').each(function() {
-    var $table = $(this);                     // This sortable table
-    var $tbody = $table.find('tbody');        // Store table body
-    var $controls = $table.find('th');        // Store table headers
-    var rows = $tbody.find('tr').toArray();   // Store array containing rows
-  
-    $controls.on('click', function() {        // When user clicks on a header
-      var $header = $(this);                  // Get the header
-      var order = $header.data('sort');       // Get value of data-sort attribute
-      var column;                             // Declare variable called column
-  
-    
-      // If selected item has ascending or descending class, reverse contents
-      if ($header.is('.ascending') || $header.is('.descending')) {  
-        $header.toggleClass('ascending descending');    
-        $tbody.append(rows.reverse());               
-      } else {                                                               
-        $header.addClass('ascending');               
-        // Remove asc or desc from all other headers
-        $header.siblings().removeClass('ascending descending'); 
-        if (compare.hasOwnProperty(order)) {  
-          column = $controls.index(this);        
-  
-          rows.sort(function(a, b) {              
-            a = $(a).find('td').eq(column).text();
-            b = $(b).find('td').eq(column).text(); 
-            return compare[order](a, b);          
-          });
-  
-          $tbody.append(rows);
-        }
-      }
-    });
-  });
 });
-   
+
