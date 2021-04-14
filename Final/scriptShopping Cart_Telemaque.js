8df94fb1465621c3dcd63efb3c2a6802509860ac
Reginald Telemaque <reggiemaque@gmail.com>
2021-04-14 15:38:54 -0400

    yea

--- Final/scriptShopping Cart_Telemaque.js
+++ Final/scriptShopping Cart_Telemaque.js
@@ -1,3 +1,7 @@
+/* Set rates + misc */
+var taxRate = 0.05;
+var shippingRate = 15.00; 
+var fadeTime = 300;
 
 
 /* Assign actions */
@@ -8,6 +12,37 @@ $('.product-quantity input').change( function() {
 $('.product-removal button').click( function() {
   removeItem(this);
 });
+
+
+/* Recalculate cart */
+function recalculateCart()
+{
+  var subtotal = 0;
+  
+  /* Sum up row totals */
+  $('.product').each(function () {
+    subtotal += parseFloat($(this).children('.product-line-price').text());
+  });
+  
+  /* Calculate totals */
+  var tax = subtotal * taxRate;
+  var shipping = (subtotal > 0 ? shippingRate : 0);
+  var total = subtotal + tax + shipping;
+  
+  /* Update totals display */
+  $('.totals-value').fadeOut(fadeTime, function() {
+    $('#cart-subtotal').html(subtotal.toFixed(2));
+    $('#cart-tax').html(tax.toFixed(2));
+    $('#cart-shipping').html(shipping.toFixed(2));
+    $('#cart-total').html(total.toFixed(2));
+    if(total == 0){
+      $('.checkout').fadeOut(fadeTime);
+    }else{
+      $('.checkout').fadeIn(fadeTime);
+    }
+    $('.totals-value').fadeIn(fadeTime);
+  });
+}
 
 
 /* Update quantity */
